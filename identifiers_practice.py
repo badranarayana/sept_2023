6aa09db06ec9ce32d60efe3712b26ad22a32404e
badranarayana <abnreddy.a@gmail.com>
2023-09-27 22:52:20 +0530

    identifier in python

---
+++ identifiers_practice.py
@@ -0,0 +1,168 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# python shell
+20 + 30
+50
+30 - 20
+10
+30/3
+10.0
+20 * 4
+80
+name = "Punn"
+last_name = "rrrr"
+name
+'Punn'
+name + last_name
+'Punnrrrr'
+name + ' ' + last_name
+'Punn rrrr'
+
+
+
+
+name = "Badra"
+name
+'Badra'
+a = 10
+b = 20
+c = 30
+a
+10
+b
+20
+c
+30
+age = 30
+location = "hyd"
+age
+30
+location
+'hyd'
+in = ll
+SyntaxError: invalid syntax
+# upper in variable name
+LOCATION = "HYD"
+LOCATION
+'HYD'
+myLocation = "Bng"
+myLocation
+'Bng'
+myage = 28
+mYAGe = 40
+myage
+28
+mYAGe
+40
+car1 = "Audi"
+Car2 = "honda"
+firstname = "Abc"
+firt_name = "Xyz"
+my_home_town = "mcl"
+myhometown = "guntur"
+firt_name
+'Xyz'
+myhometown
+'guntur'
+llocation = "hyd"
+1location = "hh"
+SyntaxError: invalid decimal literal
+back2location = "Hyd"
+a1 = 30
+1a = 30
+SyntaxError: invalid decimal literal
+123 = "hyd"
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+_location = "guntur"
+_location = "guntur" # private variables
+hsdjkskdkanakdnkadklkldld = 444
+hsdjkskdkanakdnkadklkldld
+444
+my_friend_name = "Satya"
+My_friend_name = "Peri"
+# python is casesensitive langauge, that means A and a is diffferent
+my_friend_name
+'Satya'
+My_friend_name
+'Peri'
+id(my_friend_name)
+2640552350768
+id(My_friend_name)
+2640552351088
+a = 10
+A = 30
+id(a)
+2640511042064
+id(A)
+2640511042704
+help()
+
+Welcome to Python 3.10's help utility!
+
+If this is your first time using Python, you should definitely check out
+the tutorial on the internet at https://docs.python.org/3.10/tutorial/.
+
+Enter the name of any module, keyword, or topic to get help on writing
+Python programs and using Python modules.  To quit this help utility and
+return to the interpreter, just type "quit".
+
+To get a list of available modules, keywords, symbols, or topics, type
+"modules", "keywords", "symbols", or "topics".  Each module also comes
+with a one-line summary of what it does; to list the modules whose name
+or summary contain a given string such as "spam", type "modules spam".
+
+help> keywords
+
+Here is a list of the Python keywords.  Enter any keyword to get more help.
+
+False               class               from                or
+None                continue            global              pass
+True                def                 if                  raise
+and                 del                 import              return
+as                  elif                in                  try
+assert              else                is                  while
+async               except              lambda              with
+await               finally             nonlocal            yield
+break               for                 not                 
+
+help> exit
+Help on Quitter in module _sitebuiltins object:
+
+exit = class Quitter(builtins.object)
+ |  exit(name, eof)
+ |  
+ |  Methods defined here:
+ |  
+ |  __call__(self, code=None)
+ |      Call self as a function.
+ |  
+ |  __init__(self, name, eof)
+ |      Initialize self.  See help(type(self)) for accurate signature.
+ |  
+ |  __repr__(self)
+ |      Return repr(self).
+ |  
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |  
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |  
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+
+help> 
+
+You are now leaving help and returning to the Python interpreter.
+If you want to ask for help on a particular object directly from the
+interpreter, you can type "help(object)".  Executing "help('string')"
+has the same effect as typing a particular string at the help> prompt.
+continue = 33
+SyntaxError: invalid syntax
+a = 20
+age = 20
+l = 'hyd"
+SyntaxError: unterminated string literal (detected at line 1)
+l = 'hyd'
+location = "Hyd"
+location = 'hyd'
