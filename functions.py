f2b0234a80860303bcde27e091e8ae96c08568ac
badranarayana <abnreddy.a@gmail.com>
2024-03-13 22:04:17 +0530

    Update functions.py
    params

--- functions.py
+++ functions.py
@@ -92,6 +92,92 @@ def is_exist(items, filter_item):
 print(is_exist(data, 'Aam'))
 
 
+# 13/03/2024
+"""
+
+def <func_name>(parameters/arguments):
+    .....
+    .....
+    return
+
+
+parameters/arguments:
+1) required positional args/parameters
+2) arbitrary position args(*args)
+3) default args
+4) arbitrary key ward args(**kwargs)
+
+"""
+# example on required positional args
+
+
+def add(a, b):
+    return a + b
+
+out = add(10, 10)
+print(out)
+
+def sub(a, b):
+    return a - b
+
+out = sub(10, 20)
+print(out)
+
+
+# 2) arbitrary position args(*args)
+# can pass unlimited positional args while calling function
+# *args will be a tuple object inside function
+
+
+def dynamic_add(a, b, *args):
+    # *args are optional
+    # args is available as tuple object
+    #print(args)
+
+    total = a + b
+    for val in args:
+        total += val
+    return total
+
+print(dynamic_add(20, 30, 44, 44, 44, 44))
+
+
+# Write a function to multiply
+def mul(x, y, z, *args):
+    out = x * y
+    for i in args:
+        out *= i
+    return out
+
+print(mul(20, 3, 4))
+
+
+def open_account(adhar_card, pan_card, *args):
+    if adhar_card and pan_card:
+        print("account is activated")
+        print("following documents submited")
+        print("Adhar ", adhar_card)
+        print('pan ', pan_card)
+        print("Other documents")
+        for doc in args:
+            print(doc)
+    else:
+        print("Required documents missing")
+
+open_account('444', 'dalmldl333', 'driving-3233', 'previos-loan')
+
+# car
+def build_car(wheels, engine, breaks, fuel_tank, steering,  *args):
+    if wheels and engine and breaks and fuel_tank and steering:
+        print("car ready")
+        print("with Accessories: ")
+        for item in args:
+            print(item)
+
+build_car(4, "120cc", "oil breaks", 'petrl', "yes", 'screen', 'floor mat')
+
+
+
 
 
 
