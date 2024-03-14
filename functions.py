eea05b38fa25fdb7a1e385bb8bdef13e8807ece6
badranarayana <abnreddy.a@gmail.com>
2024-03-14 20:26:08 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -177,6 +177,133 @@ def build_car(wheels, engine, breaks, fuel_tank, steering,  *args):
 build_car(4, "120cc", "oil breaks", 'petrl', "yes", 'screen', 'floor mat')
 
 
+# 14/03/2024:
+"""
+Default args:
+
+--> default args are defined with some default value
+--> default values will be passed if no value provided
+--> it uses provided value if provided, other wise default val will be passed
+
+"""
+
+def salary_hike(current_salary, hike_amount=30):
+    return current_salary + hike_amount
+
+
+print(salary_hike(100, hike_amount=40))
+# good per emp
+print(salary_hike(100))
+
+
+def check_scolership(cast='bc'):
+    if cast == 'bc':
+        print("Eligible for 40%")
+
+    if cast == 'oc':
+        print("eligible for 10%")
+
+
+check_scolership(cast='oc')
+
+
+
+def make_car(brand, model, wheel_count=4):
+    print("Brand: ", brand)
+    print("model ", model)
+    print("wheel count ", wheel_count)
+
+
+make_car('honda', 'amaze')
+
+
+make_car('honda', 'amaze', wheel_count=6)
+
+
+# arbitrary kew ward args(**kwargs)
+"""
+**kwargs : allows us to pass unlimited keyword args while calling function
+
+def <func>(<required args>, *args, var=10, **kwargs):
+      pass
+      
+Kwargs are will be available  as dict object in side function
+"""
+
+
+# student info
+def format_student(name, dob, roll_number, school_name="ZPH", **kwargs):
+    """
+
+    :param name: ? required positional arg
+    :param dob: ? required positional arg
+    :param roll_number: ? required positional arg
+    :param school_name: keyward/default arg
+    :param kwargs: keyword args
+    :return:
+    """
+
+    #print("KWARGS: ", kwargs)
+    student_data = {
+        'name': name,
+        'dob': dob,
+        'roll_number': roll_number,
+        'school_name': school_name,
+    }
+
+    student_data.update(kwargs)
+
+    return student_data
+
+sai_info = format_student('Sai', '10-03-1994', 'ABC102')
+print(sai_info)
+
+badra_info = format_student('Badra',
+                            '11-03-1994',
+                            'ABC103',
+                            school_name="Ravindra",
+                            home_address="7-92, hyd",
+                            father_name="Punna reddy",
+                            city="Guntur"
+                            )
+print(badra_info)
+
+ramu_info = format_student('ramu',
+                            '11-04-1994',
+                            'ABC103',
+                            school_name="Ravindra",
+                            home_address="7-95, hyd",
+                            father_name=" reddy",
+                            city="Guntur",
+                            reference="Ravi kumar sir",
+                            fee_amount="30000"
+                            )
+print(ramu_info)
+
+
+# lets define function with all arg types:
+# order of defining args in python
+"""
+def <func_name>(<required positional args>, <*args>, <default/key word args>, <**kwargs>):
+    pass
+
+
+"""
+
+
+def example(a, b, *args, location='Hyd', state="TG", **kwargs):
+    print("Required args ", a, b)
+    print("*args ", args)  # tuple
+    print("default/kewword args ", location, state)
+    print("**kwargs ", kwargs)  # dict
+
+print("--------------------------")
+
+example(10, 20, 30, 40, mobile_number="5513333", name='Sai', location="GUNTUR")
+
+
+
+
 
 
 
